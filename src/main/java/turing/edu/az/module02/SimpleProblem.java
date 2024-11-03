fbcae37be9f814fe7adbe5e1fc5c472332a15521
user <alimuradof93@gmail.com>
2024-11-03 20:13:46 +0400

    SimpleProblem attached

---
+++ src/main/java/turing/edu/az/module02/SimpleProblem.java
@@ -0,0 +1,12 @@
+package turing.edu.az.module02;
+import java.util.Scanner;
+public class SimpleProblem {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        System.out.print(number/10);
+        System.out.print(" ");
+        System.out.print(number%10);
+
+    }
+}
