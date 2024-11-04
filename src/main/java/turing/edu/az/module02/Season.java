b55eda0f9584226ff52461d8d0495fa9eec9fdc0
user <alimuradof93@gmail.com>
2024-11-04 23:27:49 +0400

    Season attached

---
+++ src/main/java/turing/edu/az/module02/Season.java
@@ -0,0 +1,18 @@
+package turing.edu.az.module02;
+import java.util.Scanner;
+public class Season
+{
+    public static void main(String[] args) {
+        String st1="Spring";
+        String st2="Summer";
+        String st3="Autumn";
+        String st4="Winter";
+        int a;
+        Scanner scan=new Scanner(System.in);
+        a=scan.nextInt();
+        if(a>=3 && a<=5) System.out.println(st1);
+        else if(a>=6 && a<=8) System.out.println(st2);
+        else if(a>=9 && a<=11) System.out.println(st3);
+        else if(a==1 || a==2 || a==12) System.out.println(st4);
+    }
+}
