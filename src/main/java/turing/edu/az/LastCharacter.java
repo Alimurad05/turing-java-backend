3e74d4659b4b369dec0f1c302b6c5e94fa27e6c7
user <alimuradof93@gmail.com>
2024-11-08 21:59:25 +0400

    LastCharacter attached

---
+++ src/main/java/turing/edu/az/LastCharacter.java
@@ -0,0 +1,11 @@
+package turing.edu.az;
+import  java.util.Scanner;
+public class LastCharacter {
+    public static void main(String[] args){
+    String st;
+    Scanner scan= new Scanner(System.in);
+    st=scan.nextLine();
+    char ch=st.charAt(st.length()-1);
+    System.out.println(ch);
+}
+}
