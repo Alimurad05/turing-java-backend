8051fbe275a6d9faf0ae8116a878ecfd56896f6e
user <alimuradof93@gmail.com>
2024-11-08 22:42:34 +0400

    Reverseword attached

---
+++ src/main/java/turing/edu/az/module02/Reverseword.java
@@ -0,0 +1,15 @@
+package turing.edu.az.module02;
+import java.util.Locale;
+import  java.util.Scanner;
+public class Reverseword {
+    public static void main(String[] args) {
+        String st;
+        Scanner scan = new Scanner(System.in);
+        st = scan.nextLine();
+        String st2 = st.toUpperCase();
+        for (int i = st2.length() - 1; i >= 0; i--) {
+            System.out.print(st2.charAt(i));
+        }
+
+    }
+}
